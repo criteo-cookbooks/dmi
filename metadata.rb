16c98bd68a50f99971f38903e7b2129a6377f352
Jeremy MAURO <j.mauro@criteo.com>
2017-01-12 09:48:46 +0100

    Fix the deploy job
    Change-Id: I9ef18a997a03ca4ad60d6d981e0bb0937e1a24b7

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'sre-core-infra@criteo.com'
 license          'All rights reserved'
 description      'Installs/Configures dmi'
 long_description 'Installs/Configures dmi'
-issues_url       'https://github.com/criteo-cookbooks/dmi'
-source_url       'https://github.com/criteo-cookbooks/dmi'
-version          '0.14.1'
+issues_url       'https://github.com/criteo-cookbooks/dmi' if respond_to?(:issue_url)
+source_url       'https://github.com/criteo-cookbooks/dmi' if respond_to?(:source_url)
+version          '0.14.2'
 supports         'centos'
 supports         'debian'
 supports         'windows'
