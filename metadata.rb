fcfa6ea5b125639799e34b19c9eed407512b8dea
Baptiste Courtois <b.courtois@criteo.com>
2018-07-26 18:01:24 +0200

    Fix foodcritic violations

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,16 @@
 name             'dmi'
 maintainer       'Criteo'
 maintainer_email 'sre-core-infra@criteo.com'
-license          'All rights reserved'
+license          'Apache-2.0'
 description      'Installs/Configures dmi'
 long_description 'Installs/Configures dmi'
-issues_url       'https://github.com/criteo-cookbooks/dmi' if respond_to?(:issue_url)
-source_url       'https://github.com/criteo-cookbooks/dmi' if respond_to?(:source_url)
 version          '0.14.2'
 supports         'centos'
 supports         'debian'
 supports         'windows'
+
+chef_version     '>= 12.6' if respond_to?(:chef_version)
+issues_url       'https://github.com/criteo-cookbooks/dmi' if respond_to?(:issue_url)
+source_url       'https://github.com/criteo-cookbooks/dmi' if respond_to?(:source_url)
+
 depends          'windows'
